62aee4a41852b32e3676e2a631d7929dc2d70177
master-kot <55625854+master-kot@users.noreply.github.com>
2019-11-28 15:32:03 +0300

    гамма версия домашнего задания к 5 уроку.
    Весь код, что успел сделать находится в файле RFunctions.java

--- RFunctions.java
+++ RFunctions.java
@@ -1,8 +1,7 @@
 package main.java;
 
 //import javafx.util.Pair;
-
-import jdk.internal.util.xml.impl.Pair;
+//import jdk.internal.util.xml.impl.Pair;
 
 import java.io.InputStream;
 import java.io.PrintWriter;
@@ -12,6 +11,17 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class RFunctions {
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+		RFunctions rFunctions = new RFunctions();
+        int n = 0;
+        System.out.println(rFunctions.fib(n));
+        System.out.println(rFunctions.factor(n));
+		rFunctions.reverse(in, out);
+		
+	}
 
     //функция должна находить число Фибоначчи с номер н
     public BigInteger fib(int n){
@@ -35,31 +45,94 @@ public class RFunctions {
     //и вывести в out все числа в обратном порядке без 0
     public void reverse(Scanner in, PrintWriter out){
         //TODO
-        
+        int x = in.nextInt();
+		if (x == 0) return;
+		reverse();
+		try {
+			out.println(x);
+		}
     }
 
     //функция должна отсортировать массив слиянием
-    public ArrayList<Integer> mergeSort(ArrayList<Integer> list){
+    //public ArrayList<Integer> mergeSort(ArrayList<Integer> list){
+	//реализована сортировка слиянием в массив, 
+	//сортировка списков сделать не успел(
+	public int[] mergeSort(int[] arrayAll){
         //TODO
-        Integer[] arrayOfList = new Integer[list.size()];
-        list.toArray(arrayOfList);
-
+		//заготовка для метода работающего с списком
+		//int [] arrayAll = new int[list.size()];
+		//System.arraycopy(list.toArray(), 0, arrayAll, 0, list.size());
+		
+		if (list == null) {
+			return null;
+		}
+				
+		if (arrayAll.length < 2) {
+			return arrayAll;
+		}
+		int [] arrayLeft = new int[arrayAll.length / 2];
+		System.arraycopy(arrayAll, 0, arrayLeft, 0, arrayAll.length / 2);
+		
+		int [] arrayRight = new int[arrayAll.length - arrayAll.length / 2];
+		System.arraycopy(arrayAll, arrayAll.length / 2, arrayRight, 0, arrayAll.length - arrayAll.length / 2);
+		
+		arrayLeft = mergeSort(arrayLeft);
+		arrayRight = mergeSort(arrayRight);
+		
+		return mergeArray(arrayLeft, arrayRight);
+    }
+	
+	public class Pair <T element> {
+		private T left;
+		private T right;
+		
+		public Pair (T left, T right){
+			this.left = left;
+			this.right = right;
+		}
+		
+		public T getRight {
+			return right;
+		}
+		
+		public T getLeft {
+			return left;
+		}
+	}
+	
+    //функция должна разбить список пополам (2 2 для 4 или 3 2 для 5)
+	//не успел реализовать, однако планирую использовать наработку из 
+	//метода mergeSort реализованного выше для массива
+    Pair<ArrayList<Integer>, ArrayList<Integer>> split(ArrayList<Integer> list){
+        //TODO
         return null;
     }
-
-
-    //функция должна разбить список пополам (2 2 для 4 или 3 2 для 5)
-/*    Pair<ArrayList<Integer>, ArrayList<Integer>>
-    split(ArrayList<Integer> list){
-        //TODO
-        return null;
-    }*/
 
     //функция должна вернуть отсортированный список, сливаемый из двух отсортированных
-    public ArrayList<Integer> merge(ArrayList<Integer> left, ArrayList<Integer> right){
-        //TODO
-        return null;
-    }
+    //public ArrayList<Integer> merge(ArrayList<Integer> left, ArrayList<Integer> right){
+    //заготовка метода работающего со списком, пока что работающий с массивами
+	//TODO
+	public int [] mergeArray(int [] arrayLeft, int [] arrayRight) {
+		int [] mergedArray = int[arrayLeft.length + arrayRight.length];
+		int positionLeft = 0, positionRight = 0;
+
+		for (int i = 0; i < mergedArray.length; i++) {
+			if (positionLeft == arrayLeft.length){
+				mergedArray[i] = arrayRight[i - positionRight];
+				positionRight++;
+			} else if (positionRight == arrayRight.length) {
+				mergedArray[i] = arrayLeft[i - positionLeft];
+				positionLeft++;
+			} else if (arrayLeft[i - positionLeft] < arrayRight[i - positionRight]) {
+				mergedArray[i] = arrayLeft[i - positionLeft];
+				positionRight++;
+			} else {
+				mergedArray[i] = arrayRight[i - positionRight];
+				positionLeft++;
+			}
+		}
+		return mergedArray;
+	}
 
     static boolean [] used;
     static int cnt = 0;
