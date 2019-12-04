564a1006527004f969a4df3a45fd020ec83afe60
master-kot <55625854+master-kot@users.noreply.github.com>
2019-12-05 00:24:10 +0300

    альфа версия домашней работы к уроку 6
    Переписал стринговые и нестринговые методы инордер, преордер.
    Попытался сделать метод delete, писал сам, как его видел.
    дополнительно ввел 2 переменные:    parent и parentSide для отслеживания местоположения ;
    Косяки его работы:
    1) при удалении root удаляются все члены дерева, остается только root. Почему?
    2) нельзя удалить листья, то есть последние члены, не имеющие потомков. Почему?
    3) средние члены дерева удаляются без проблем, дерево сохраняется

---
+++ main/java/Main.java
@@ -0,0 +1,31 @@
+package main.java;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BST<Integer> tree = new BST<>();
+        tree.add(5);
+        tree.add(1);
+        tree.add(7);
+        tree.add(3);
+        tree.add(10);
+        tree.add(12);
+        tree.add(-3);
+        for (int i = 0; i < 13; i++) {
+            System.out.println(i + " " + tree.find(i));
+        }
+
+//        System.out.println(tree.inOrder());
+        System.out.println(tree.preOrder());
+        tree.remove(1);
+        System.out.println(tree.preOrder());
+        tree.remove(7);
+        System.out.println(tree.preOrder());
+        tree.remove(10);
+        System.out.println(tree.preOrder());
+        tree.remove(5);
+        System.out.println(tree.preOrder());
+    }
+}
